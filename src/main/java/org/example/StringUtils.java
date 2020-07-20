4470022619d4fb4962656dfa4b2b53afcde7b0d5
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-07-20 16:45:20 +0200

    UniqueVowel tests ok

--- src/main/java/org/example/StringUtils.java
+++ src/main/java/org/example/StringUtils.java
@@ -9,7 +9,9 @@ public class StringUtils {
      * @return Chaine avec uniquement des voyelles
      */
     public static String vowels(String candidate) {
+
         if (candidate.equals("") || candidate.equals(null)) {
+
             throw new IllegalArgumentException();
         }
         String vowels = "";
@@ -21,6 +23,31 @@ public class StringUtils {
         }
         return vowels;
     }
+
+    /**
+     * Renvoie la chaine formée par les voyelles d'une chaine de caractères
+     * @return Chaine avec uniquement des voyelles
+     */
+    public static String uniqueVowels(String candidate) {
+
+        if (candidate.equals("") || candidate.equals(null)) {
+
+            throw new IllegalArgumentException();
+        }
+
+        String vowels = "";
+        char[] letters = candidate.toCharArray();
+        for (int i = 0; i < candidate.length(); i++) {
+
+            if (ALL_VOWELS.indexOf(letters[i]) >= 0
+                && !(vowels.indexOf(letters[i]) >= 0)) {
+
+                vowels += letters[i];
+            }
+        }
+        return vowels;
+    }
+
 
 }
 
--- src/test/java/org/example/StringUtilsTest.java
+++ src/test/java/org/example/StringUtilsTest.java
@@ -28,13 +28,33 @@ public class StringUtilsTest {
         assertEquals("Oae", StringUtils.vowels("Orange"));
     }
 
-    // Problème trouvé : quand la premier caractère de l'entrée est une voyelle,
-    // celle-ci ne ressort pas.
-
-
     @Test(expected = IllegalArgumentException.class)
     public void testException() {
 
         StringUtils.vowels("");
+    }
+
+    @Test
+    public void testAAA() {
+
+        assertEquals("A", StringUtils.uniqueVowels("AAA"));
+    }
+
+    @Test
+    public void testAbracadabra() {
+
+        assertEquals("a", StringUtils.uniqueVowels("abracadabra"));
+    }
+
+    @Test
+    public void testHmmm() {
+
+        assertEquals("", StringUtils.uniqueVowels("hmmm"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testException2() {
+
+        StringUtils.uniqueVowels("");
     }
 }
